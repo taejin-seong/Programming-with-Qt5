c6f898a3307db2a29d49a6cf2ee35c0b5b45a158
taejin-seong <sungtj0130@naver.com>
2022-07-10 03:29:11 +0900

    p41

---
+++ 03. QT Basic/01_BasicWidget/03_QCommandLInkButton/widget.cpp
@@ -0,0 +1,22 @@
+#include "widget.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+{
+    setFixedSize(QSize(300, 100));
+
+    cmmBtn = new QCommandLinkButton("Vision", "Vision Project", this);
+    cmmBtn->setFlat(true);
+
+    connect(cmmBtn, SIGNAL(clicked()), this, SLOT(clickFunc()));
+}
+
+void Widget::clickFunc()
+{
+    qDebug("QCommandLinkButton Clcik. ");
+}
+
+Widget::~Widget()
+{
+}
+
---
+++ 03. QT Basic/01_BasicWidget/03_QCommandLInkButton/widget.h
@@ -0,0 +1,20 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QCommandLinkButton>
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private:
+    QCommandLinkButton *cmmBtn;
+
+private slots:
+    void clickFunc();
+};
+#endif // WIDGET_H
